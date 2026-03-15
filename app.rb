6e46783ae4e5b64e7785eab5686d8e407762da1a
devmuturi <muturinjonge09@gmail.com>
2026-03-15 15:22:50 +0300

    Implemented the router DSL

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
+require_relative "lib/Router/router"
 # create a Rack-compliant web application
 class App
-  def call(_env)
+  attr_reader :router
+
+  def initialize
+    @router = Router.new
+
+    router.get("/") { "Fred's Blog" }
+
+    router.get("/articles") { "All Articles" }
+
+    router.get("/articles/1") { "First Articles" }
+  end
+
+  def call(env)
     # a hash containing response headers
     headers = {
       "Content-Type": "text/html"
     }
 
-    # an array containing the HTML response string
-    response = ["<h1>Hello World!</h1>"]
+    response_html = router.build_response(env["REQUEST_PATH"])
 
-    [200, headers, response]
+    [200, headers, [response_html]]
   end
 end
---
+++ lib/Router/router.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Router system
+class Router
+  def initialize
+    @routes = {}
+  end
+
+  def get(path, &blk)
+    @routes[path] = blk
+  end
+
+  def build_response(path)
+    handler = @routes[path] || -> { "no route found for #{path}" }
+    handler.call
+  end
+end
